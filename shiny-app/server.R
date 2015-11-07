38b535e9fcc86b89a637586c8c8bf297c8a94e20
Aron Lindberg <aron.lindberg@case.edu>
2015-11-07 12:04:32 -0500

    loading the data

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -2,13 +2,7 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
+  emergency2013 <- read.csv("./data/emergency2013.csv")
   
   output$distPlot <- renderPlot({
    plot(faithful$eruptions, faithful$waiting)
