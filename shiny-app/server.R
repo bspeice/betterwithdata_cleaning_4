675ca16f02cffa085e6f2ffeaa0214cc27264b19
Aron Lindberg <aron.lindberg@case.edu>
2015-11-07 13:50:31 -0500

    add plot

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -5,10 +5,10 @@ shinyServer(function(input, output) {
   emergency2013 <- read.csv("./data/emergency2013.csv")
   
   output$mytable1 <- renderDataTable({
+      emergency2013[, input$show_vars, drop = FALSE]
+  })
+  output$myplot <- renderPlot({
     library(ggplot2)
-    emergency2013[, input$show_vars, drop = FALSE]
-    
-      
     library(corrplot)
     pairs(emergency2013[, input$show_vars, drop = FALSE])
   })
